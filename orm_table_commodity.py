b1887d5b181a585199f1900e09081687ece1a14e
Taerghods <sanazzt88@gmail.com>
2024-11-20 05:56:45 +0330

    add: py file

---
+++ orm_table_commodity.py
@@ -0,0 +1,91 @@
+import psycopg2
+
+class PostgresConnection:
+    _conn = None
+
+    @classmethod
+    def connect(cls):
+        if cls._conn:
+            return cls._conn
+        cls._conn = psycopg2.connect(
+            dbname='Commodity',
+            user='postgres',
+            password='s.1988',
+            host='localhost',
+            port='5432')
+        cls._conn.autocommit = True
+        return cls._conn
+
+    @classmethod
+    def get_cursor(cls):
+        cursor = cls.connect().cursor()
+        return cursor
+
+    @classmethod
+    def create_table(cls, table_name:str, *columns):
+        try:
+            part = ",".join(columns)
+            with cls.get_cursor() as cursor:
+                cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({part});")
+                print(f"table {table_name} created successfully.")
+        except Exception as e:
+            print(f"Error create table{table_name}: {e}")
+
+    @classmethod
+    def insert(cls, table_name: str, columns: list, data: list):
+        try:
+            columns_str = ",".join(columns)
+            values_str = ",".join(["%s"] * len(columns))
+            with cls.get_cursor() as cursor:
+                cursor.executemany(f"INSERT INTO {table_name} ({columns_str}) VALUES ({values_str})", data)
+                print(f"inserting into {table_name} completed successfully.")
+        except Exception as e:
+            print(f"Error inserting into {table_name}: {e}")
+
+    @classmethod
+    def update(cls, table_name:str, id_colum:str, id_value:int, **columns):
+        try:
+            part = ",".join([f"{key} = %s" for key in columns.keys()])
+            part_value = list(columns.values()) + [id_value]
+            with cls.get_cursor() as cursor:
+                cursor.execute(f"UPDATE {table_name} SET {part} WHERE {id_colum} = %s;", part_value)
+                print(f"Update {table_name} completed successfully.")
+        except Exception as e:
+            print(f"Error Update {table_name}: {e}")
+
+
+    @classmethod
+    def delete(cls, table_name:str, id_colum:str, id_value:int,):
+        try:
+            part_value = [id_value]
+            with cls.get_cursor() as cursor:
+                cursor.execute(f"DELETE FROM {table_name} WHERE {id_colum} = %s;", part_value)
+                print(f"Delete {table_name} completed successfully.")
+        except Exception as e:
+            print(f"Error Delete {table_name}: {e}")
+
+    @classmethod
+    def select_all(cls, table_name: str) -> object:
+        try:
+            with cls.get_cursor() as cursor:
+                cursor.execute(f"SELECT * FROM {table_name};")
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(row)
+        except Exception as e:
+            print(f"Error fetch all {table_name}: {e}")
+
+
+    @classmethod
+    def select(cls, query:str) -> object:
+        try:
+            with cls.get_cursor() as cursor:
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                if rows:
+                    for row in rows:
+                        print(row)
+                else:
+                    print("No rows found!")
+        except Exception as e:
+            print(f"Error fetch : {e}")
---
+++ query_commodity.py
@@ -0,0 +1,55 @@
+from orm_table_commodity import PostgresConnection
+
+table_inventory = PostgresConnection.create_table(
+    "Inventory",
+    '''inventory_id SERIAL PRIMARY KEY,  
+              name VARCHAR(255) NOT NULL,  
+              quantity INT NOT NULL,
+              price INT NOT NULL,
+              type VARCHAR(50) NOT NULL, 
+              weight FLOAT,
+              dimensions VARCHAR(255),
+              file_size FLOAT,
+              download_link VARCHAR(255),
+              date_added TIMESTAMP DEFAULT now() ''')
+
+'''q1: Insert new items into the database.'''
+# insert_inventory = PostgresConnection.insert("Inventory",
+#                                              ['name', 'quantity', 'price', 'type', 'weight', 'dimensions', 'file_size', 'download_link'],
+#                                              [('Laptop', 10, 50000, 'PhysicalItem', 2.5, '30x20x2', None, None),
+#                                                   ('Smartphone', 15, 40000, 'PhysicalItem', 0.3, '15x7x0.8', None, None),
+#                                                   ('Headphones', 25, 7500, 'PhysicalItem', 0.2, '15x15x5', None, None),
+#                                                   ('Smartwatch', 30, 12500, 'PhysicalItem', 0.5, '4x4x1', None, None),
+#                                                   ('Tablet', 20, 25000, 'PhysicalItem', 0.8, '25x15x1', None, None),
+#                                                   ('Software License', 25, 199.99, 'DigitalItem', 200.5, None, None, None),
+#                                                   ('E-book', 100, 5, 'DigitalItem', None, None, None, None),
+#                                                   ('Online Course', 200, 1500, 'DigitalItem', None, None, None, 'http://example.com/course'),
+#                                                   ('Music Album', 150, 800, 'DigitalItem', None, None, None, 'http://example.com/music'),
+#                                                   ('Video Game', 75, 3000, 'DigitalItem', None, None, None, 'http://example.com/game')])
+
+print('''q2: Update the stock quantity of an item.''')
+q2 = PostgresConnection.update("Inventory", "inventory_id", 1, price=50000)
+
+print('''q3: Retrieve all items in the inventory.''')
+q3 = PostgresConnection.select_all("Inventory")
+
+print('''q4: Find items with stock quantities less than 20.''')
+q4 = PostgresConnection.select("SELECT * FROM Inventory WHERE quantity < 20")
+
+print('''q5: Get the total value of all items in stock by multiplying price and quantity then summing the results''')
+q5 = PostgresConnection.select("SELECT sum(quantity * price) FROM Inventory")
+
+print('''q6: Find items by their type''')
+q6 = PostgresConnection.select("SELECT * FROM Inventory WHERE type='PhysicalItem'")
+
+print('''q7: Get the average price of all items in the inventory.''')
+q7 = PostgresConnection.select("SELECT avg(price) FROM Inventory")
+
+print('''q8: Delete an item from the inventory by its id.''')
+# q8 = PostgresConnection.delete("Inventory", "inventory_id",10)
+
+print('''q9: Update the price of an item based on a given id and new price.''')
+q9 = PostgresConnection.update("Inventory", "inventory_id", 2, price=45000)
+
+print('''q10: Retrieve items that have been in stock for the longest time''')
+q10 = PostgresConnection.select("SELECT name, date_added FROM Inventory order by date_added asc limit 10")
